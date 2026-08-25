97401c68d1853df2efec855ce31b12fecdff2102
李天琦 <ltq65143989@163.com>
2026-08-25 18:00:32 +0800

    Initial commit

---
+++ src/main/java/com/cherry/cherryaiagent/CherryAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.cherry.cherryaiagent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CherryAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CherryAiAgentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cherry/cherryaiagent/CherryAiAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cherry.cherryaiagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CherryAiAgentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
